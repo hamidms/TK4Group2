7115973f1a3c074ef733234681ce22ca3769e5b3
chrisnadhe <40015276+chrisnadhe@users.noreply.github.com>
2024-01-20 21:46:13 +0700

    fix minor typo

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -54,7 +54,7 @@ const LoginScreen = () => {
         />
         <View style={{flex: 1, justifyContent: 'flex-end', paddingBottom: 10}}>
           <Text style={{alignSelf: 'center'}}>
-            &copy; 2023, Group 2 - Mobile Programming
+            &copy; 2024, Group 2 - Mobile Programming
           </Text>
         </View>
       </View>
