accf6b8ce504656a0aae2d34d95b6ad3bf3d2757
chrisnadhe <40015276+chrisnadhe@users.noreply.github.com>
2024-01-20 21:30:15 +0700

    changes to JS

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import LoginScreen from './Screens/LoginScreen';
+import HomeScreen from './Screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </View>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 2,
+  },
+});
+
+export default App;
