d3102f125a0a5765d3df5c0a2f83f956de84de31
chrisnadhe <40015276+chrisnadhe@users.noreply.github.com>
2024-01-20 21:44:17 +0700

    add google login

--- App.js
+++ App.js
@@ -2,11 +2,18 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Config from 'react-native-config';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 import LoginScreen from './Screens/LoginScreen';
 import HomeScreen from './Screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
+
+GoogleSignin.configure({
+  webClientId: Config.WEB_CLIENT_ID,
+  offlineAccess: true,
+});
 
 const App = () => {
   return (
--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -3,6 +3,8 @@ import {View, Text} from 'react-native';
 import LoginForm from '../Components/LoginForm';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {useNavigation} from '@react-navigation/native';
+import {GoogleSigninButton} from '@react-native-google-signin/google-signin';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 const LoginScreen = () => {
   const navigation = useNavigation();
@@ -19,13 +21,37 @@ const LoginScreen = () => {
     console.log('Login attempt with email:', email, 'password:', password);
   };
 
+  const handleGoogleLogin = async () => {
+    try {
+      const userInfo = await GoogleSignin.signIn();
+      console.log('User info:', userInfo);
+      navigation.navigate('Home');
+      // Handle successful login (e.g., store user data, navigate to app)
+    } catch (error) {
+      console.error('Login error:', error);
+      // Handle login errors
+    }
+  };
+
   return (
     <PaperProvider>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text style={{fontSize: 24, fontWeight: 'bold', marginBottom: 20, marginTop: 10}}>
+        <Text
+          style={{
+            fontSize: 24,
+            fontWeight: 'bold',
+            marginBottom: 20,
+            marginTop: 10,
+          }}>
           Login
         </Text>
         <LoginForm onSubmit={handleLogin} />
+        <GoogleSigninButton
+          style={{width: 192, height: 48, marginTop: 20}}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+          onPress={handleGoogleLogin}
+        />
         <View style={{flex: 1, justifyContent: 'flex-end', paddingBottom: 10}}>
           <Text style={{alignSelf: 'center'}}>
             &copy; 2023, Group 3 - Mobile Programming
